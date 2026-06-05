fe8b17cb36e06a369fe3e62b507281e03892dc99
YoussefMahmoud14 <you2sef1221@gmail.com>
2026-06-05 22:39:41 +0300

    Update Geo1.cpp

--- Geometry/Geo1.cpp
+++ Geometry/Geo1.cpp
@@ -1,4 +1,5 @@
-/// TRANSFORMATIONS , ANGLES , Lines , 
+/// TRANSFORMATIONS , ANGLES , Lines , Segments , Polygon
+
 
 #define ld long double
 const ld EPS = 1e-12 ;
@@ -46,7 +47,7 @@ pt prep(pt p){
     return {-p.y, p.x};
 }
 
-//////////////// TRANSFORMATIONS ////////////////////////
+//////////////////////////////////// TRANSFORMATIONS ///////////////////////////////////////////////
 
 // Translates a point by a given vector.
 // v: translation vector, p: point to be moved.
@@ -121,7 +122,7 @@ bool inAngle(pt a, pt b, pt c, pt p) {
     return (abp >= 0 && acp <= 0) ^ (abc < 0);
 }
 
-////////////////////////// Lines //////////////////////////////
+/////////////////////////////////////////////////// Lines //////////////////////////////////////////////////////////
 
 struct line{
 
@@ -233,3 +234,68 @@ line bisector(line l1, line l2, bool interior) {
     inter(...)   // line-line intersection
     bisector(...)// angle bisector between two lines
  */
+
+
+//////////////////////////////////////////  SEGMENTS   //////////////////////////////////////////
+ 
+bool inDisk(pt a, pt b, pt p) {
+    return dot(a-p, b-p) <= EPS;
+}
+ 
+bool onSegment(pt a, pt b, pt c){
+    return orient(a, b, c) == 0 && inDisk(a, b, c);
+}
+
+bool properInter(pt a, pt b, pt c, pt d, pt &out) {
+    T oa = orient(c,d,a),
+            ob = orient(c,d,b),
+            oc = orient(a,b,c),
+            od = orient(a,b,d);
+// Proper intersection exists iff opposite signs
+    if (sgn(oa)*sgn(ob) < 0 && sgn(oc)*sgn(od) < 0) {
+        out = (a*ob - b*oa) / (ob-oa);
+        return true;
+    }
+    return false;
+}
+
+// check the intersection of two Segments
+set<pair<ld,ld>> inters(pt a, pt b, pt c, pt d) {
+    set<pair<ld,ld>> s;
+    pt out;
+    if(a == c || a == d){
+        s.insert(make_pair(a.x, a.y));
+    }
+    if(b == c || b == d){
+        s.insert(make_pair(b.x, b.y));
+    }
+    if(s.size()) return s;
+ 
+    if (properInter(a,b,c,d,out)) return {make_pair(out.x, out.y)};
+    if (onSegment(c,d,a)) s.insert(make_pair(a.x, a.y));
+    if (onSegment(c,d,b)) s.insert(make_pair(b.x, b.y));
+    if (onSegment(a,b,c)) s.insert(make_pair(c.x, c.y));
+    if (onSegment(a,b,d)) s.insert(make_pair(d.x, d.y));
+ 
+    return s;
+}
+
+// calc the distance between point and segment
+ld segPoint(pt a, pt b, pt p) {
+    if (a != b) {
+        line l(a,b);
+        if (l.cmpProj(a,p) && l.cmpProj(p,b)) // if closest to projection
+            return l.dist(p); // output distance to line
+    }
+    return min(abs(p-a), abs(p-b)); // otherwise distance to A or B
+}
+
+// calc the distance between two segments
+ld segSeg(pt a, pt b, pt c, pt d) {
+    pt dummy;
+    if (properInter(a,b,c,d,dummy))
+        return 0;
+    return min({segPoint(a,b,c), segPoint(a,b,d),
+                segPoint(c,d,a), segPoint(c,d,b)});
+}
+
