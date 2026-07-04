15ff2fce876a7d085c66f7e2ffac245e60cf772e
YoussefMahmoud14 <you2sef1221@gmail.com>
2026-07-04 18:30:02 +0300

    Create isPrime.cpp

---
+++ Number Theory/isPrime.cpp
@@ -0,0 +1,10 @@
+// Checks primality in O(sqrt(n))
+bool is_prime(ll n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (ll i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
